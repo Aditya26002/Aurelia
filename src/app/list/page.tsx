4d0e37afdc051235c27204ec97133d5e61419e43
Aditya Daksh <aditya2012058@akgec.ac.in>
2024-07-31 16:48:34 +0530

    Add lazy loading for images on ListPage

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,7 +1,44 @@
-import React from "react";
+import Filter from "@/components/Filter";
+import ProductList from "@/components/ProductList";
+import { wixClientServer } from "@/lib/wixClientServer";
+import Image from "next/image";
+import React, { Suspense } from "react";
 
-const ListPage = () => {
-  return <div>ListPage</div>;
+const ListPage = async ({ searchParams }: { searchParams: any }) => {
+  const wixClient = await wixClientServer();
+  const cat = await wixClient.collections.getCollectionBySlug(
+    searchParams.cat || "all-products"
+  );
+  return (
+    <div className="px-4 md:px-7 lg:px-14 xl:px-28 2xl:px-60 relative">
+      {/* CAMPAIGN */}
+      <div className="hidden bg-pink-50 px-4 sm:flex justify-between h-64">
+        <div className="w-2/3 flex flex-col items-center justify-center gap-8">
+          <h1 className="text-4xl font-semibold leading-[48px] text-gray-700">
+            Grab upto 50% off <br /> Selected Products
+          </h1>
+          <button className="rounded-3xl bg-lama text-white w-max py-3 px-5 text-sm">
+            Buy Now
+          </button>
+        </div>
+        <div className="relative w-1/3">
+          <Image src="/woman.png" alt="" fill className="object-contain" />
+        </div>
+      </div>
+      {/* FILTER */}
+      <Filter />
+      {/* PRODUCTS */}
+      <h1 className="mt-12 text-xl font-semibold">Shoes For You!</h1>
+      <Suspense fallback={"Loading..."}>
+        <ProductList
+          categoryId={
+            cat.collection?._id || "00000000-000000-000000-000000000001"
+          }
+          searchParams={searchParams}
+        />
+      </Suspense>
+    </div>
+  );
 };
 
 export default ListPage;
