597269c54b86bf7b325c65b2a7400fa4c732895e
Aditya Daksh <aditya2012058@akgec.ac.in>
2024-09-05 16:52:41 +0530

    Add pagination to ProductList component

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -4,6 +4,9 @@ import DOMPurify from "isomorphic-dompurify";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import Pagination from "./Pagination";
+
+const PRODUCT_PER_PAGE = 8;
 
 const ProductList = async ({
   categoryId,
@@ -16,11 +19,36 @@ const ProductList = async ({
 }) => {
   const wixClient = await wixClientServer();
 
-  const res = await wixClient.products
+  const productQuery = wixClient.products
     .queryProducts()
+    .startsWith("name", searchParams?.name || "")
     .eq("collectionIds", categoryId)
-    .limit(limit || 20)
-    .find();
+    .hasSome(
+      "productType",
+      searchParams?.type ? [searchParams.type] : ["physical", "digital"]
+    )
+    .gt("priceData.price", searchParams?.min || 0)
+    .lt("priceData.price", searchParams?.max || 999999)
+    .limit(limit || PRODUCT_PER_PAGE)
+    .skip(
+      searchParams?.page
+        ? parseInt(searchParams.page) * (limit || PRODUCT_PER_PAGE)
+        : 0
+    );
+
+  if (searchParams?.sort) {
+    const [sortType, sortBy] = searchParams.sort.split(" ");
+    console.log(sortType, sortBy);
+
+    if (sortType === "asc") {
+      productQuery.ascending(sortBy);
+    }
+    if (sortType === "desc") {
+      productQuery.descending(sortBy);
+    }
+  }
+
+  const res = await productQuery.find();
 
   return (
     <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
@@ -34,41 +62,46 @@ const ProductList = async ({
             <Image
               src={product.media?.mainMedia?.image?.url || "/product.png"}
               alt=""
-              sizes="25vw"
               fill
-              className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity ease-in duration-500"
+              sizes="25vw"
+              className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
             />
             {product.media?.items && (
               <Image
                 src={product.media?.items[1]?.image?.url || "/product.png"}
                 alt=""
-                sizes="25vw"
                 fill
+                sizes="25vw"
                 className="absolute object-cover rounded-md"
               />
             )}
           </div>
           <div className="flex justify-between">
             <span className="font-medium">{product.name}</span>
-            <span className="font-semibold">
-              {product.price?.formatted?.discountedPrice}
-            </span>
+            <span className="font-semibold">₹{product.price?.price}</span>
           </div>
-          <div
-            className="text-sm text-gray-500"
-            dangerouslySetInnerHTML={{
-              __html: DOMPurify.sanitize(
-                product.additionalInfoSections?.find(
-                  (section: any) => section.title === "shortDesc"
-                )?.description || ""
-              ),
-            }}
-          ></div>
-          <button className="rounded-2xl ring-1 ring-lama text-lama py-2 px-4 text-xs hover:bg-lama hover:text-white">
+          {product.additionalInfoSections && (
+            <div
+              className="text-sm text-gray-500"
+              dangerouslySetInnerHTML={{
+                __html: DOMPurify.sanitize(
+                  product.additionalInfoSections.find(
+                    (section: any) => section.title === "shortDesc"
+                  )?.description || ""
+                ),
+              }}
+            ></div>
+          )}
+          <button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4 text-xs hover:bg-lama hover:text-white">
             Add to Cart
           </button>
         </Link>
       ))}
+      <Pagination
+        currentPage={res.currentPage || 0}
+        hasPrev={res.hasPrev()}
+        hasNext={res.hasNext()}
+      />
     </div>
   );
 };
