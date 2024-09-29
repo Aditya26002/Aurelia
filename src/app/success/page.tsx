a927b4449a33f9a0c6130e566c8864be5546958b
Aditya Daksh <aditya2012058@akgec.ac.in>
2024-09-29 15:42:38 +0530

    Update SuccessPage component to add suspense and fix typo

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 import { useRouter, useSearchParams } from "next/navigation";
-import React, { useEffect } from "react";
-import Confettti from "react-confetti";
+import React, { useEffect, Suspense } from "react";
+import Confetti from "react-confetti";
+
 const SuccessPage = () => {
   const searchParams = useSearchParams();
   const router = useRouter();
@@ -18,16 +19,21 @@ const SuccessPage = () => {
       clearTimeout(timer);
     };
   }, [orderId, router]);
+
   return (
-    <div className="flex flex-col gap-6 items-center justify-center h-{calc(100vh-180px)}">
-      <Confettti width={2000} height={1000} />
-      <h1 className="text-6xl text-gray-700">Succesfull</h1>
-      <h2 className="text-xl font-medium">
-        We sent the invcoive to your email
-      </h2>
-      <h3>You are being redirected to order page...</h3>
+    <div className="flex flex-col gap-6 items-center justify-center h-[calc(100vh-180px)]">
+      <Confetti width={2000} height={1000} />
+      <h1 className="text-6xl text-gray-700">Successful</h1>
+      <h2 className="text-xl font-medium">We sent the invoice to your email</h2>
+      <h3>You are being redirected to the order page...</h3>
     </div>
   );
 };
 
-export default SuccessPage;
+const SuccessPageWrapper = () => (
+  <Suspense fallback={<div>Loading...</div>}>
+    <SuccessPage />
+  </Suspense>
+);
+
+export default SuccessPageWrapper;
